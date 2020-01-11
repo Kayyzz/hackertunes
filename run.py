452c6ff96ff9b7cf10ac5d23123d61912df24e64
Wenqin Ye <wenqinye@dhcp-59-177.cu-wireless-faculty.carleton.ca>
2020-01-11 15:26:02 -0500

    second commit

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from flask import Flask
-app = Flask(__name__,
-    static_folder='./public',
-    template_folder='./static')
+from templates import app
 
-from templates.hello.views import hello_blueprint
-app.register_blueprint(hello_blueprint)
+#Load this config object for development mode
+app.config.from_object('configurations.DevelopmentConfig')
+app.run()
--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const resolve = require('path').resolve;
+
 const config = {
  devtool: 'eval-source-map',
  entry: __dirname + '/js/index.jsx',
@@ -7,24 +8,21 @@ const config = {
       path: resolve('../public'),
       filename: 'bundle.js',
       publicPath: resolve('../public')
- },
+},
  resolve: {
   extensions: ['.js','.jsx','.css']
  },
  module: {
-    rules: [
-    {
-     test: /\.jsx?/,
-     loader: 'babel-loader',
-     exclude: /node_modules/,
-     query:{
-        presets: ['react','es2015']
-      }
-    },
-    {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?modules'
-    }]
- },
+  rules: [
+  {
+   test: /\.jsx?/,
+   loader: 'babel-loader',
+   exclude: /node_modules/
+  },
+  {
+         test: /\.css$/,
+         loader: 'style-loader!css-loader?modules'
+  }]
+ }
 };
 module.exports = config;
